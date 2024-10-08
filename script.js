c7c41bd8145bbe5cc2922163c88db376b50abb19
Pedro Henrique <pedrohenriqueoliveiradosanjos@gmail.com>
2024-10-07 21:54:27 -0300

    Js interminado
    Variáveis criadas.
    Variáveis ativadas.
    próximo passo: criar sistema de divisão

---
+++ script.js
@@ -0,0 +1,18 @@
+function calcular() {
+let nota1, nota2, nota3 , divisor;
+
+nota1 = document.getElementById("txt_var1").value;
+nota2 = document.getElementById("txt_var2").value;
+nota3 = document.getElementById("txt_var3").value;
+divisor = document.getElementById("divisor").value;
+
+
+
+
+
+
+
+
+
+
+}
