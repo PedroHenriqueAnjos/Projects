bf68df6e5591c7ffd0d709652b95ea5442697d00
Pedro Henrique <pedrohenriqueoliveiradosanjos@gmail.com>
2024-10-11 20:48:35 -0300

    Média pronta
    HTML pronto

--- ExercicioMÉDIA/script.js
+++ ExercicioMÉDIA/script.js
@@ -1,20 +1,17 @@
 function calcular() {
-let nota1, nota2, nota3 , resultado;
+let nota1, nota2, resultado;
 
 nota1 = document.getElementById('txt_var1').value;
 nota2 = document.getElementById('txt_var2').value;
 nota3 = document.getElementById('txt_var3').value;
 
-nota1, nota2, nota3, divisor = Number(nota1, nota2, nota3, divisor, resultado)
+nota1= Number(nota1)
+nota2= Number(nota2)
+nota3 = Number(nota3)
 
+resultado = (nota1 + nota2 + nota3)/3;
 
-
-
-
-
-
-
-
+document.getElementById('p').innerHTML = (resultado);
 
 
 }
