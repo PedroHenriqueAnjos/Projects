92a83a1669cefd89723676593487bdf487f32899
iwnexx <pedrohenriqueoliveiradosanjos@gmail.com>
2024-11-13 21:50:29 -0300

    Exercício finalizado

---
+++ Exercício ano/script.js
@@ -0,0 +1,24 @@
+function ver(){
+
+let contador
+
+contador = document.getElementById('txt_ano').value;
+
+contador=Number(contador);
+
+if(contador % 4==0 ){
+       
+        alert('é bissexto');}
+    
+        else{
+            
+        
+            alert('não é bissexto');
+        }
+    }
+
+
+
+
+
+
